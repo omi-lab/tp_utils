9bbc3aff8bca25e1e1e7a7228fc084b19a95c929
Tom Paynter <tompaynter@tdpe.co.uk>
2018-10-09 13:33:27 +0100

    Save POIs as part of a scene.

--- inc/tp_utils/JSONUtils.h
+++ inc/tp_utils/JSONUtils.h
@@ -9,6 +9,7 @@
 #define TPJSONString tp_utils::getJSONValue<std::string>
 #define TPJSONInt    tp_utils::getJSONValue<int>
 #define TPJSONSizeT  tp_utils::getJSONValue<size_t>
+#define TPJSONInt64T tp_utils::getJSONValue<int64_t>
 #define TPJSONFloat  tp_utils::getJSONValue<float>
 #define TPJSONDouble tp_utils::getJSONValue<double>
 #define TPJSONBool   tp_utils::getJSONValue<bool>
