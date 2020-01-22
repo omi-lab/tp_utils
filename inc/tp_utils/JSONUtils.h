323cf812423322d8c3ce94dd7a8d4d19aa920820
Tom Paynter <tompaynter@tdpe.co.uk>
2020-01-22 14:45:43 +0000

    Cleaning up lose ends.

--- inc/tp_utils/JSONUtils.h
+++ inc/tp_utils/JSONUtils.h
@@ -5,16 +5,17 @@
 
 #include "json.hpp"
 
-#define TPJSON       tp_utils::getJSONValue<nlohmann::json>
-#define TPJSONString tp_utils::getJSONValue<std::string>
-#define TPJSONInt    tp_utils::getJSONValue<int>
-#define TPJSONSizeT  tp_utils::getJSONValue<size_t>
-#define TPJSONInt64T tp_utils::getJSONValue<int64_t>
-#define TPJSONFloat  tp_utils::getJSONValue<float>
-#define TPJSONDouble tp_utils::getJSONValue<double>
-#define TPJSONBool   tp_utils::getJSONValue<bool>
-#define TPJSONList   tp_utils::getJSONStringList
-#define TPJSONArray  tp_utils::getJSONArray
+#define TPJSON        tp_utils::getJSONValue<nlohmann::json>
+#define TPJSONString  tp_utils::getJSONValue<std::string>
+#define TPJSONInt     tp_utils::getJSONValue<int>
+#define TPJSONSizeT   tp_utils::getJSONValue<size_t>
+#define TPJSONInt64T  tp_utils::getJSONValue<int64_t>
+#define TPJSONUint16T tp_utils::getJSONValue<uint16_t>
+#define TPJSONFloat   tp_utils::getJSONValue<float>
+#define TPJSONDouble  tp_utils::getJSONValue<double>
+#define TPJSONBool    tp_utils::getJSONValue<bool>
+#define TPJSONList    tp_utils::getJSONStringList
+#define TPJSONArray   tp_utils::getJSONArray
 
 namespace tp_utils
 {
