9af23e9c969503d306341b6a2d4de7d34a17f014
Tom Paynter <tompaynter@tdpe.co.uk>
2021-01-18 13:30:56 +0000

    Added support for Alpine Linux.

--- src/SignalHandler.cpp
+++ src/SignalHandler.cpp
@@ -11,12 +11,8 @@ namespace tp_utils
 using SignalHandlerT = _crt_signal_t;
 #elif defined TP_EMSCRIPTEN
 using SignalHandlerT = sighandler_t;
-#elif defined TP_OSX
-using SignalHandlerT = void (*)(int);
-#elif defined TP_IOS
-using SignalHandlerT = void (*)(int);
 #else
-using SignalHandlerT = __sighandler_t;
+using SignalHandlerT = void (*)(int);
 #endif
 
 //##################################################################################################
