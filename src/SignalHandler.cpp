b915de3fdd241ac0d8b393dd8d0eb1caae8351d4
Tom Paynter <tompaynter@tdpe.co.uk>
2021-02-01 13:15:20 +0000

    Added a crash handler.

--- src/SignalHandler.cpp
+++ src/SignalHandler.cpp
@@ -1,8 +1,11 @@
 #include "tp_utils/SignalHandler.h"
+#include "tp_utils/StackTrace.h"
 #include "tp_utils/MutexUtils.h"
+#include "tp_utils/FileUtils.h"
 #include "tp_utils/RefCount.h"
 
 #include <csignal>
+#include <iostream>
 
 namespace tp_utils
 {
@@ -21,8 +24,15 @@ struct SignalHandler::Private
   TP_REF_COUNT_OBJECTS("tp_utils::SignalHandler::Private");
   TP_NONCOPYABLE(Private);
 
-  SignalHandlerT signt;
+  bool exitOnInt;
+
+  SignalHandlerT sigint;
   SignalHandlerT sigterm;
+
+  SignalHandlerT sigabrt;
+  SignalHandlerT sigfpe;
+  SignalHandlerT sigill;
+  SignalHandlerT sigsegv;
 
   TPMutex mutex{TPM};
   TPWaitCondition waitCondition;
@@ -30,7 +40,8 @@ struct SignalHandler::Private
   static Private* instance;
 
   //################################################################################################
-  Private()
+  Private(bool exitOnInt_):
+    exitOnInt(exitOnInt_)
   {
     instance = this;
   }
@@ -44,8 +55,22 @@ struct SignalHandler::Private
   //################################################################################################
   static void exitWake(int sig)
   {
-    TP_UNUSED(sig);
+    std::cout << "Signal caught: " << sig << '\n';
+    if(instance->exitOnInt)
+    {
+      printStackTrace();
+      tp_utils::writeTextFile("crash.txt", formatStackTrace());
+      exit(sig);
+    }
     instance->waitCondition.wakeAll();
+  }
+
+  //################################################################################################
+  static void exitNow(int sig)
+  {
+    std::cout << "Fatal signal caught: " << sig << '\n';
+    printStackTrace();
+    exit(sig);
   }
 };
 
@@ -53,18 +78,29 @@ struct SignalHandler::Private
 SignalHandler::Private* SignalHandler::Private::instance{nullptr};
 
 //##################################################################################################
-SignalHandler::SignalHandler():
-  d(new Private)
+SignalHandler::SignalHandler(bool exitOnInt):
+  d(new Private(exitOnInt))
 {
-  d->signt = std::signal(SIGINT, Private::exitWake);
+  d->sigint  = std::signal(SIGINT , Private::exitWake);
   d->sigterm = std::signal(SIGTERM, Private::exitWake);
+
+  d->sigabrt = std::signal(SIGABRT, Private::exitNow);
+  d->sigfpe  = std::signal(SIGFPE , Private::exitNow);
+  d->sigill  = std::signal(SIGILL , Private::exitNow);
+  d->sigsegv = std::signal(SIGSEGV, Private::exitNow);
 }
 
 //##################################################################################################
 SignalHandler::~SignalHandler()
 {
-  std::signal(SIGINT, d->signt);
+  std::signal(SIGINT , d->sigint );
   std::signal(SIGTERM, d->sigterm);
+
+  std::signal(SIGABRT, d->sigabrt);
+  std::signal(SIGFPE , d->sigfpe );
+  std::signal(SIGILL , d->sigill );
+  std::signal(SIGSEGV, d->sigsegv);
+
   delete d;
 }
 
