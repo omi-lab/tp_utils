bd412b79f72dfd93b381d2d363501cf553376f30
Alex Blinov <alexander@omi.so>
2024-01-11 15:40:21 +0000

    AI API networking

--- inc/tp_utils/BarrierWithCounter.h
+++ inc/tp_utils/BarrierWithCounter.h
@@ -4,6 +4,8 @@
 #include <mutex>
 #include <atomic>
 #include <condition_variable>
+#include <functional>
+#include <queue>
 
 namespace tp_utils {
 
@@ -19,22 +21,44 @@ public:
     ++count;
   }
 
-  void release()
+  void unlock()
   {
     --count;
     cv.notify_all();
   }
 
+  void run(std::function<void()> const& job)
+  {
+    std::scoped_lock<std::mutex> lk(cv_m);
+    jobs.push(job);
+  }
+
   void wait()
   {
     std::unique_lock<std::mutex> lk(cv_m);
-    cv.wait(lk, [&]{ return count == 0;} );
+    cv.wait(lk, [&]{
+      while(!jobs.empty()){
+        auto job = jobs.back();
+        jobs.pop();
+        lk.unlock();
+        job();
+        lk.lock();
+      }
+      return count == 0;
+    });
+    // dependent threads finished
+    // completing last posted jobs
+    while(!jobs.empty()){
+      jobs.back()();
+      jobs.pop();
+    }
   }
 
-  std::mutex cv_m; // mutex is open occasionally it can be used in threads
+  std::mutex cv_m; // mutex is public - sometimes it can be usefull in threads
 private:
   std::condition_variable cv;
   std::atomic_int count = 0;
+  std::queue<std::function<void()>> jobs;
 };
 
 }
